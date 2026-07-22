6c97a00488ddb75588757bf0653467e8da4712f7
spaswan-coder <162099278+spaswan-coder@users.noreply.github.com>
2026-07-22 18:02:33 +0530

    Time: 2 ms (38.18%), Space: 8.6 MB (21.02%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+    
+        int ans=0;
+        while(x!=0){
+            int dig=x%10;
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && dig > 7))
+                return 0;
+
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && dig < -8))
+                return 0;
+            ans=ans*10+dig;
+            x=x/10;
+        }
+        return ans;
+
+        
+    }
+};
